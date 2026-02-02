78dcaaf686068227319adda8321475c13f3d9765
hawlet musbah <hawletmusbah00@gmail.com>
2026-02-02 14:06:24 +0530

    Move repository files to correct package folder

--- src/main/java/com/aau/se/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/aau/se/expensetracker/repository/ExpenseRepository.java
@@ -1,4 +1,18 @@
 package com.aau.se.expensetracker.repository;
 
-public class ExpenseRepository {
+import com.aau.se.expensetracker.model.Expense;
+import com.aau.se.expensetracker.util.DataAccessException;
+
+import java.util.List;
+
+/**
+ * Data access contract for expenses (Dependency Inversion: depend on abstraction).
+ */
+public interface ExpenseRepository {
+
+    void save(Expense expense) throws DataAccessException;
+
+    List<Expense> loadAll() throws DataAccessException;
+
+    void delete(String expenseId) throws DataAccessException;
 }
